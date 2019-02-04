92c48b21ccc5b3c7448d490fc3bca30ffff584e2
Roger Mei <rogermei2017@u.northwestern.edu>
2019-02-04 16:59:15 -0600

    Add files via upload

--- student_code.py
+++ student_code.py
@@ -128,7 +128,64 @@ class KnowledgeBase(object):
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
         # Student code goes here
-        
+        if not isinstance(fact_or_rule, Fact) and not isinstance(fact_or_rule, Rule):
+            return None
+
+        if isinstance(fact_or_rule, Fact):
+            fact_retract = self._get_fact(fact_or_rule)
+            if fact_retract.asserted and fact_retract.supported_by:
+                return None
+            if not fact_retract.supported_by:
+                # remove fact_retract from supported_by of other facts 
+                for f_sup in fact_retract.supports_facts:
+                    for fr in f_sup.supported_by:
+                        if fact_retract in fr:
+                            real_r = self._get_rule(fr[1])
+                            real_r.supports_facts.remove(f_sup)
+                            real_f_sup = self._get_fact(f_sup)
+                            real_f_sup.supported_by.remove(fr)
+                        if not real_f_sup.supported_by:
+                            self.kb_retract(real_f_sup)
+                # remove fact_retract from supported_by of other rules
+                for r_sup in fact_retract.supports_rules:
+                    for fr in r_sup.supported_by:
+                        if fact_retract in fr:
+                            real_r = self._get_rule(fr[1])
+                            real_r.supports_rules.remove(r_sup)
+                            real_r_sup = self._get_rule(r_sup)
+                            real_r_sup.supported_by.remove(fr)
+                        if not real_r_sup.asserted:
+                            self.kb_retract(real_r_sup)
+                self.facts.remove(fact_retract)
+                
+        if isinstance(fact_or_rule, Rule):
+            if fact_or_rule.asserted:
+                return None
+            else:
+                rule_retract = self._get_rule(fact_or_rule)
+                if not rule_retract.supported_by:
+                    # remove rule_retract from supported_by of other facts 
+                    for f_sup in rule_retract.supports_facts:
+                        for fr in f_sup.supported_by:
+                            if rule_retract in fr:
+                                real_f = self._get_fact(fr[0])
+                                real_f.supports_facts.remove(f_sup)
+                                real_f_sup = self._get_fact(f_sup)
+                                real_f_sup.supported_by.remove(fr)
+                            if not real_f_sup.supported_by:
+                                self.kb_retract(real_f_sup)
+                    # remove rule_retract from supported_by of other rules
+                    for r_sup in rule_retract.supports_rules:
+                        for fr in r_sup.supported_by:
+                            if rule_retract in fr:
+                                real_f = self._get_fact(fr[0])
+                                real_f.supports_facts.remove(r_sup)
+                                real_r_sup = self._get_rule(r_sup)
+                                real_r_sup.supported_by.remove(fr)
+                            if not real_r_sup.asserted:
+                                self.kb_retract(real_r_sup)
+                    self.rules.remove(rule_retract)
+                
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
@@ -144,5 +201,31 @@ class InferenceEngine(object):
         """
         printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
             [fact.statement, rule.lhs, rule.rhs])
+        
         ####################################################
         # Student code goes here
+        # match to first lhs statement
+        bindings_match = match(fact.statement, rule.lhs[0])
+
+        if bindings_match:
+            # If there is more than 1 statement in the lhs of rule
+            lhs_instantiated = []
+            rhs_instantiated = instantiate(rule.rhs, bindings_match)
+            if (len(rule.lhs) > 1):
+                for statement in rule.lhs[1:]:
+                    # instantiate makes a new statement 
+                    # (either lhs or rhs) of rule with variable bindings
+                    lhs_instantiated.append(instantiate(statement, bindings_match))
+                
+            # create new fact if there's only 1 lhs statement 
+            if not len(lhs_instantiated):    
+                fact_inferred = Fact(rhs_instantiated, supported_by=[[fact, rule]])
+                rule.supports_facts.append(fact_inferred)
+                fact.supports_facts.append(fact_inferred)    
+                kb.kb_assert(fact_inferred)
+            # create new rule if more than 1 lhs statement    
+            else:    
+                rule_inferred = Rule([lhs_instantiated, rhs_instantiated], supported_by=[[fact, rule]])
+                rule.supports_rules.append(rule_inferred)
+                fact.supports_rules.append(rule_inferred)    
+                kb.kb_assert(rule_inferred)
